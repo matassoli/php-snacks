f655c7ec1589858702238a292d8c5655a3d07b14
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-05-04 14:44:28 +0200

    snacks

---
+++ snack1.php
@@ -0,0 +1,38 @@
+<?php
+    $matches = [
+  [
+    "squadra_casa" => "Olimpia Milano",
+    "punti_casa" => 55,
+    "squadra_ospite"  => "Cantù",
+    "punti_ospite" => 60,
+  ],
+  [
+    "squadra_casa" => "Squadra1",
+    "punti_casa" => 60,
+    "squadra_ospite"  => "Squadra2",
+    "punti_ospite" => 49,
+  ],
+  [
+    "squadra_casa" => "Squadra3",
+    "punti_casa" => 54,
+    "squadra_ospite"  => "Squadra4",
+    "punti_ospite" => 55,
+  ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Snack-1</title>
+  </head>
+  <body>
+
+<?php
+foreach ($matches as $match) {
+  echo "{$match["squadra_casa"]} - {$match["squadra_ospite"]} | {$match["punti_casa"]} - {$match["punti_ospite"]}</br>";}
+?>
+
+  </body>
+</html>
---
+++ snack2.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+  $name = $_GET["name"];
+  $mail = $_GET["mail"];
+  $age = $_GET["age"];
+?>
+
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  <title>Snack-2</title>
+</head>
+
+<body>
+
+  <p> <?php echo $name ?> </p>
+  <p> <?php echo $mail ?> </p>
+  <p> <?php echo $age ?> </p>
+
+
+
+
+  <?php
+  if (strlen($name) > 3
+    && is_numeric($age) == true
+    && strpos($mail, "@") == true
+    && strpos($mail, ".") == true) {
+      echo "Accesso riuscito";
+  } else {
+      echo "Accesso negato";
+  }
+  ?>
+
+</body>
+</html>
